102da634aa3bdc85d9166da131b1e32bc4f6f24a
Jay <4a490063@stust.edu.tw>
2020-12-26 16:03:49 +0800

    feat: add progress bar

--- component/Progress/index.tsx
+++ component/Progress/index.tsx
@@ -1,7 +1,20 @@
-import { Wrapper } from './styled';
+import { useEffect, useState, useCallback, useRef, MouseEvent } from 'react';
+import { Wrapper, Bar } from './styled';
+import { useAudioPosition } from 'react-use-audio-player';
 
 const Progress = () => {
-	return <Wrapper></Wrapper>
+  const { percentComplete, seek } = useAudioPosition({ highRefreshRate: true });
+  const [width, setWidth] = useState('0%');
+
+  useEffect(() => {
+    setWidth(`${percentComplete}%`);
+  }, [percentComplete]);
+
+  return (
+    <Wrapper>
+      <Bar width={width} />
+    </Wrapper>
+  );
 };
 
 export default Progress;
--- component/Progress/styled.tsx
+++ component/Progress/styled.tsx
@@ -1,8 +1,22 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-	width: 300px;
-	background: #d0d8e6;
-	height: 6px;
-	margin-top: 10px;
+  width: 300px;
+  background: #d0d8e6;
+  height: 6px;
+  margin-top: 10px;
+  position: relative;
+`;
+
+interface IBarProps {
+  width: string;
+}
+
+export const Bar = styled.div`
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: ${({ width }: IBarProps) => width};
+  height: inherit;
+  background: #000;
 `;
